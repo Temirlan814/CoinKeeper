f914d9f9a4d12b1531cc75b3afc6329988e5f5c0
Temirlan814 <t.temirlan.2004@mail.ru>
2025-05-11 23:47:19 +0500

    fix folders

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,12 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+// @ts-ignore
+import path from "path"
+// @ts-ignore
+import { fileURLToPath } from "url"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   plugins: [react()],
@@ -10,6 +17,11 @@ export default defineConfig({
   css: {
     modules: {
       localsConvention: "camelCase",
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
     },
   },
 })
